d4cfbdbb109e989d9b49b2bca9dde49dbcb21c79
HNE74 <30633070+HNE74@users.noreply.github.com>
2020-12-31 17:11:45 +0100

    removed stuff

--- SprBgCollision/bak/macros.asm
+++
@@ -1,8 +0,0 @@
-;******************************************************
-;*** Custom defined macros
-;***
-;*** by Noltisoft in 2021 
-;*** The code is unlicensed and might be used and/or
-;*** modified by any interested parties.
-;*****************************************************
-
