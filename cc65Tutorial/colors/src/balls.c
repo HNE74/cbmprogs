28ab8792aeb55d7f0a658919f077fe69a96fd7f8
HNE74 <30633070+HNE74@users.noreply.github.com>
2021-08-03 21:47:25 +0200

    Multiple bouncing balls

--- cc65Tutorial/colors/src/balls.c
+++ cc65Tutorial/colors/src/balls.c
@@ -3,10 +3,13 @@
 #include <peekpoke.h>
 #include <time.h>
 
+#define BALL_COUNT 5
+#define OBSTACLE_COUNT 10
+
 const int VIC = 0xD000; // VIC start address
 const int SCREEN = 0X400; // Screen ram address
-int x=1, y=1;   // Ball coordinates
-int dx=1, dy=1; // Ball movement vector
+int x[BALL_COUNT], y[BALL_COUNT]; // Ball coordinates
+int dx[BALL_COUNT], dy[BALL_COUNT]; // Ball movement vectors
 
 // Wait for raster line
 void rasterWait(int cnt) {
@@ -23,23 +26,23 @@ void rasterWait(int cnt) {
 
 // Add horizontal movement vector to ball. Invert vector if
 // horizontal screen border reached.
-void addX() {
-    x+=dx;
-    if(x<0 || x>39) {
-        dx=-dx;
-        addX();
-        addX();
+void addX(int ballNo) {
+    x[ballNo]+=dx[ballNo];
+    if(x[ballNo]<0 || x[ballNo]>39) {
+        dx[ballNo]=-dx[ballNo];
+        addX(ballNo);
+        addX(ballNo);
     }
 }
 
 // Add vertical movement vector to ball. Invert vector if
 // vertical screen border reached.
-void addY() {
-    y+=dy;
-    if(y<0 || y>24) {
-        dy=-dy;
-        addY();
-        addY();
+void addY(int ballNo) {
+    y[ballNo]+=dy[ballNo];
+    if(y[ballNo]<0 || y[ballNo]>24) {
+        dy[ballNo]=-dy[ballNo];
+        addY(ballNo);
+        addY(ballNo);
     }
 }
 
@@ -57,39 +60,67 @@ void prepareScreen() {
     POKE(VIC+33, 0);
 
     printf("%c", 147);
-    for(l=0; l<10; l++) {
+    for(l=0; l<OBSTACLE_COUNT; l++) {
         POKE(SCREEN+rand() % 1000, 166);
     }
 }
 
 // Check background collision
-void checkBgColl() {
-    if(PEEK(SCREEN+(x+dx)+40*(y+dy)) == 166) {
+void checkBgColl(int ballNo) {
+    if(PEEK(SCREEN+(x[ballNo]+dx[ballNo])+40*(y[ballNo]+dy[ballNo])) != 32) {
         if(rand() % 2 == 0) {
-            dx=-dx;
+            dx[ballNo]=-dx[ballNo];
         }
         else {
-            dy=-dy;
+            dy[ballNo]=-dy[ballNo];
+        }
+    }
+}
+
+// Initialize ball data 
+void initBalls() {
+    int ballNo;
+    for (ballNo=0; ballNo<BALL_COUNT; ballNo++) {
+        x[ballNo] = rand() % 40;
+        if (x[ballNo] < 20) {
+            dx[ballNo] = 1;
+        }
+        else {
+            dx[ballNo] = -1;
+        }
+
+        y[ballNo] = rand() % 24;
+        if (y[ballNo] < 12) {
+            dy[ballNo] = 1;
+        } 
+        else {
+            dy[ballNo] = -1;
         }
     }
 }
 
 // Main routine
 int main() {
-    int oldx, oldy;
+    int oldx, oldy, i;
 
     srand(time(NULL));
+    initBalls();
     prepareScreen();
 
-    drawChar(x,y,81);
+    for(i=0; i<BALL_COUNT; i++) {    
+        drawChar(x[i],y[i],81);
+    }
+
     do {
         rasterWait(2);
-        oldx=x; oldy=y;
-        checkBgColl();   
-        addX();
-        addY();
-        drawChar(x,y,81);
-        drawChar(oldx,oldy,32);
+        for(i=0; i<BALL_COUNT; i++) {
+            oldx=x[i]; oldy=y[i];
+            checkBgColl(i);   
+            addX(i);
+            addY(i);
+            drawChar(x[i],y[i],81);
+            drawChar(oldx,oldy,32);
+        }
     }
     while(1);
 
