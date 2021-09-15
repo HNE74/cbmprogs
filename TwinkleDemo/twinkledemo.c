68143271fbd62dfa525fbcf69064acc9619f37c9
HNE74 <30633070+HNE74@users.noreply.github.com>
2021-09-15 09:38:47 +0200

    Reorganized code

--- TwinkleDemo/twinkledemo.c
+++ TwinkleDemo/twinkledemo.c
@@ -1,32 +1,7 @@
-//***********************************************
-//*** Charset change example, code extraced from
-//*** https://github.com/StewBC/cc65-Chess
-//***********************************************
-
-#include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <conio.h>
-#include <cc65.h>
-#include <c64.h>
-#include <string.h>
 #include <peekpoke.h>
-
-/*-----------------------------------------------------------------------*/
-// System locations
-#define VIC_BASE_RAM			(0x8000)
-#define SCREEN_RAM				((char*)VIC_BASE_RAM+0x0400)
-#define CHARMAP_RAM				((char*)VIC_BASE_RAM+0x0800)
-#define COLOUR_RAM				((char*)0xd800)
-#define MEM_KRNL_PRNT			((char*)0x288)
-
-/*-----------------------------------------------------------------------*/
-// Structure for character definition
-struct charDef {
-    int chrcode;
-	char lines[8];
-};
-typedef struct charDef CharDef;
+#include "chardef/chardef.h"
 
 // Star characters defintion array
 CharDef starChars[4];
@@ -40,17 +15,6 @@ int star[4][8] = {
 };
 
 /*-----------------------------------------------------------------------*/
-
-// Modify character
-void modchar(CharDef *chrdef) {
-	int offset;
-	int i;
-
-	offset = chrdef->chrcode * 8;
-	for (i=0; i<8; i++) {
-		CHARMAP_RAM[offset+i] = chrdef->lines[i];
-	}
-}
 
 // Define star characters
 void defStars() {
@@ -76,22 +40,6 @@ void rotateStarDefs() {
 	modchar(&starChars[3]);
 }
 
-// Move user definable font to ram
-void createUserFont(void) {
-    // Set up a user defined font, and move the screen to the appropriate position
-	CIA2.ddra |= 0x03;
-	CIA2.pra = (CIA2.pra & 0xfc) | (3-(VIC_BASE_RAM / 0x4000));
-	VIC.addr = ((((int)(SCREEN_RAM - VIC_BASE_RAM) / 0x0400) << 4) + (((int)(CHARMAP_RAM - VIC_BASE_RAM) / 0x0800) << 1));
-	//VIC.ctrl2 |= 16; multicolor chars disabled
-	*MEM_KRNL_PRNT = (int)SCREEN_RAM / 256;
-
-	// Copy the standard font to where the redefined char font will live
-	CIA1.cra = (CIA1.cra & 0xfe);
-	*(char*)0x01 = *(char*)0x01 & 0xfb;
-	memcpy(CHARMAP_RAM,COLOUR_RAM,256*8);
-	*(char*)0x01 = *(char*)0x01 | 0x04;
-	CIA1.cra = (CIA1.cra | 0x01);
-}
 
 // Prepares the screen
 void prepareScreen() {
